58b6af35aed739b1e3dcc57e3fb2aac9cfda6704
Davgom16 <2025039@student.cct.ie>
2025-11-11 15:58:21 +0000

    Commit_#1
    Creation of the repository
    Student number: 2025039
    Student Name: David Gomez
    CA2 algorithms and constructs
    2nd semester

---
+++ src/ca2_hdip2_2025039_david_gomez/CA2_HDip2_2025039_David_Gomez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca2_hdip2_2025039_david_gomez;
+
+/**
+ *
+ * @author dav_g
+ */
+public class CA2_HDip2_2025039_David_Gomez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
