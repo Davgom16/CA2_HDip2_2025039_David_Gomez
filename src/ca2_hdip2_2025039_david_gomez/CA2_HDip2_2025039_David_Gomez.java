235cf5ff636b17e0cb81a5f2209aa707b8edfab0
Davgom16 <2025039@student.cct.ie>
2025-11-11 17:09:04 +0000

    Commit_#2
    creating the code to read the document with the dummy info.

--- src/ca2_hdip2_2025039_david_gomez/CA2_HDip2_2025039_David_Gomez.java
+++ src/ca2_hdip2_2025039_david_gomez/CA2_HDip2_2025039_David_Gomez.java
@@ -4,6 +4,10 @@
  */
 package ca2_hdip2_2025039_david_gomez;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author dav_g
@@ -14,7 +18,31 @@ public class CA2_HDip2_2025039_David_Gomez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner inputScanner = new Scanner(System.in); // Scanner for user input
+
+        System.out.print("Enter the file name to read: ");
+        String fileName = inputScanner.nextLine();
+
+        // The text file is in the project root folder
+        File file = new File(fileName);
+
+        try (Scanner fileScanner = new Scanner(file)) { // Scanner for file
+
+            // Read the file line by line
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                
+            }
+        // message showing that  the doc was read.    
+            System.out.println("Document was successfully read");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found!");
+            e.printStackTrace();
+        }
     }
+    
+    
     
 }
